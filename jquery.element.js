0f75043e011ffc1f6ef5a2be0e86cae9ec448b09
Johan Gorter <johan@johangorter.com>
2013-02-13 21:28:16 +0100

    Overhead

--- jquery.element.js
+++ jquery.element.js
@@ -1,3 +1,14 @@
+/* Copyright 2013, Johan Gorter
+ * This file is part of jquery.element.
+ * jquery.element is free software: you can redistribute it and/or modify it under the terms of the GNU Lesser General
+ * Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option)
+ * any later version. jquery.element is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser 
+ * General Public License for more details. You should have received a copy of the GNU Lesser General Public License
+ * along with jquery.element. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
 (function ($) {
   "use strict";
 
